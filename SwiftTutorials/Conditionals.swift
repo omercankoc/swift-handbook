0e36e01cf57123b95dc67ce5e5718f725f945501
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-09-16 16:40:41 +0300

    Update Conditionals.swift

--- SwiftTutorials/Conditionals.swift
+++ SwiftTutorials/Conditionals.swift
@@ -2,25 +2,38 @@ import UIKit
 
 var midterm : Float = 65
 var final : Float = 55
-var mean : Float = (midterm + final)/2
+var score : Float = (midterm + final)/2
+
+var modulus = Int(score) % 2
 
 if(final >= 50){
-    if(mean >= 50 && mean < 55){
+    if(score >= 50 && score < 55){
         print("DC")
-    } else if(mean >= 55 && mean < 60){
+    } else if(score >= 55 && score < 60){
         print("DD")
-    } else if(mean >= 60 && mean < 68){
+    } else if(score >= 60 && score < 68){
         print("CC")
-    } else if(mean >= 68 && mean < 76){
+    } else if(score >= 68 && score < 76){
         print("CB")
-    } else if(mean >= 76 && mean < 84){
+    } else if(score >= 76 && score < 84){
         print("BB")
-    } else if(mean >= 84 && mean < 92){
+    } else if(score >= 84 && score < 92){
         print("BA")
-    } else if (mean >= 92 && mean <= 100){
+    } else if (score >= 92 && score <= 100){
         print("AA")
     }
 }
 else {
    print("FF")
+}
+
+switch modulus {
+    case 0:
+        print("Even")
+        break // Bu kosul saglandiginda donguyu kir...
+    case 1:
+        print("Odd")
+        break // Bu kosul saglandiginda donguyu kir...
+    default:
+        print("ANY!")
 }
