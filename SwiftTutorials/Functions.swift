ebc6a90686ce63ccdbc32ceffbec469bad9faf47
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-09-16 13:38:20 +0300

    Create Functions.swift

---
+++ SwiftTutorials/Functions.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// Parametresiz fonksiyon
+func Message(){
+    print("Hello Swift!")
+}
+
+// Method Overloading
+func Message(name : String){
+    print("Hello",name)
+}
+
+// Method Overloading
+func Message(name : String, surname : String){
+  print("Hello",name,surname)
+}
+
+func Calculate(base : Int, exponent : Int) -> Int {
+    var result : Int = 1
+    var iterator : Int = 1
+    while iterator <= exponent{
+        result *= base
+        iterator += 1
+    }
+    return result
+}
+
+Message()
+Message(name : "Omer")
+Message(name : "Omer", surname : "Koc")
+print(Calculate(base : 2,exponent : 2))
