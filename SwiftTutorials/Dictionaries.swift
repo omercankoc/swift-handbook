8b04d35b7663e7bce35d635d3bea679863442075
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2020-09-16 13:40:42 +0300

    Create Dictionaries.swift

---
+++ SwiftTutorials/Dictionaries.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+// Index yerine key-value deger eslesmesi ile verileri tutar.
+var numbers : [Int:String] = [1:"One",2:"Two",3:"Three",4:"Four",5:"Five"]
+
+print(numbers)
+print(numbers[2]!)
