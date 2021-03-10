55a967c2467b8e97f760803813500cfcb693ac9a
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-03-10 18:57:46 +0300

    Update Loops.swift

--- SwiftTutorial/Loops.swift
+++ SwiftTutorial/Loops.swift
@@ -7,11 +7,21 @@ var iterator : Int = 0
 
 // While Loop:
 while iterator <= limit{
+    
+    //Iterator 10 degerine esit ise donguyu kes.
+    if(iterator == 10){
+        break;
+    }
     base.append(iterator)
     iterator+=1
 }
 
 // For Loop:
 for item in base{
+    
+    // Item 5 degerine esit ise bu degeri islemeyi atla.
+    if(item == 5){
+        continue;
+    }
     print(item,"^2 =",base[item]*base[item])
 }
