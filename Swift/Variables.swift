9c51617affa6a8c7b1c9f95d6493e7618358b664
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-04-29 22:21:01 +0300

    Update Variables.swift

--- Swift/Variables.swift
+++ Swift/Variables.swift
@@ -1,37 +1,34 @@
-import UIKit
-
-import UIKit
-
-var str = "Hello, Swift!"
-print(str)
-
 // Degisken Tanimlama Yontemleri:
-// var/let variableName = value -> definition and initialization
-// var/let variableName : variableType -> definition
-// var/let variableName : variableType = value -> definition and initialization
-// var : degisken tanimlamak icin kullanilir. (mutable)
+// 1 - var/let variableName = value -> definition and initialization
+// 2 - var/let variableName : variableType -> definition
+// 3 - var/let variableName : variableType = value -> definition and initialization
+
+// var : Degisken tanimlamak icin kullanilir. (Mutable)
 var mutableVariable : String = "iOS"
 print("Mutable -> var mutableValue : String = \(mutableVariable)")
 
-// let : sabit tanimlamak icin kullanilir. (inmutable)
+// let : Sabit tanimlamak icin kullanilir. (Inmutable)
 let inmutableValue : String = "Swift"
 print("Imutable -> var inmutableValue : String = \(inmutableValue)")
 
 // Integer
 var count : Int = 9
-print("Integer to Float : \(Float(count))")   // Integer to Float
+print("Integer to Float : \(Float(count))")   // Integer to Float Type
 print("Integer to Double : \(Double(count))") // Integer to Double
 print("Integer to String : \(String(count))") // Integer to String
+
 // Float:
 var euler : Float = 2.7182818
 print("Float to Int : \(Int(euler))")         // Float to Integer
 print("Float to Double : \(Double(euler))")   // Float to Double
 print("Float to String : \(String(euler))")   // Float to String
+
 // Double:
 var pi : Double = 3.14159265358979
 print("Double to Float : \(Float(pi))")      // Double to Float
 print("Double to Double : \(Int(pi))")       // Double to Integer
 print("Double to String : \(String(pi))")    // Double to String
+
 // Bool:
 var logic : Bool = true
 
