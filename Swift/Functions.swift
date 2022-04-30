106dd642dbd2d6a01570556167b4ba4afc0841c4
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-04-30 17:17:39 +0300

    Update Functions.swift

--- Swift/Functions.swift
+++ Swift/Functions.swift
@@ -25,9 +25,24 @@ func Calculate(base : Int, exponent : Int) -> Int {
     return result
 }
 
+func minMax(array: [Int]) -> (min: Int, max: Int) {
+    var currentMin = array[0]
+    var currentMax = array[0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+        }
+    }
+    return (currentMin, currentMax)
+}
+
 Message()
 Message(name : "Omer")
 Message(name : "Omer", surname : "Koc")
 print(Calculate(base : 2,exponent : 2))
 var exponent = Calculate(base: 3, exponent: 3)
 print(exponent)
+
+print(minMax(array: [1,1,2,3,5,8,13,21]))
