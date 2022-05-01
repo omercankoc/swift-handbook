9baebc98fafb102d04e6d66f43fadbe8ace906b2
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2022-05-01 16:53:56 +0300

    Update Structs.swift

--- Swift/Structs.swift
+++ Swift/Structs.swift
@@ -1,19 +1,18 @@
-import UIKit
-
 struct Language {
     var name : String
     var country : String
     var family : String
     
-    // Degistirilebilir.
+    // Degistirilebilir. Belirli bir yöntemde yapısal değişiklik yapılmak istenirse kullanılır.
     mutating func mutatingCountry(){
         self.country = "\(country) Rep."
     }
 }
 
 var german = Language(name : "German", country: "Germany, Austria", family : "Hint-Europe")
+var russia = Language(name : "Russian", country: "Russia", family : "Hint-Europe")
 let turkish = Language(name : "Turkish", country: "Turkey", family: "Altai")
 
 print("\(german.name) \(german.country) \(german.family)")
-german.mutatingCountry()
-print("\(german.name) \(german.country) \(german.family)")
+russia.mutatingCountry()
+print("\(russia.name) \(russia.country) \(russia.family)")
