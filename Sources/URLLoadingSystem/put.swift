1cda2c32e1c77593271f385530de8a48b66b09f7
Ömer Koç <63169919+omercankoc@users.noreply.github.com>
2024-02-21 12:51:11 +0300

    Update put.swift

--- Sources/URLLoadingSystem/put.swift
+++ Sources/URLLoadingSystem/put.swift
@@ -1,1 +1,38 @@
+func putImage(endpoint: String, image: ImageModel) async throws {
+    guard let url = URL(string: endpoint) else { throw RestAPIError.invalidURL }
 
+    var request = URLRequest(url: url, timeoutInterval: 10)
+    request.addValue(clientCode, forHTTPHeaderField: "ClientCode")
+    request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+    request.addValue(token, forHTTPHeaderField: "Authorization")
+    request.httpMethod = "PUT"
+
+    let image : [String: Any?] = [
+        "metadata": [
+            "dimension": image.metadata.dimension ?? nil,
+            "width": image.metadata.width ?? nil,
+            "height": image.metadata.height ?? nil,
+            "horizontalResolution": image.metadata.horizontalResolution ?? nil,
+            "verticalResolution": image.metadata.verticalResolution ?? nil,
+            "bitDepth": image.metadata.bitDepth ?? nil,
+            "representation": image.metadata.representation ?? nil
+        ] as [String: Any?],
+        "date": image.date ?? nil,
+        "location": image.location ?? nil,
+        "id": image.id ?? nil,
+        "image": image.image.jpegData(compressionQuality: 0.25)?.base64EncodedString() ?? nil
+    ] as [String: Any?] 
+
+    var encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    encoder.outputFormatting = .withoutEscapingSlashes
+    let body = try encoder.encode(image)
+    request.httpBody = body
+
+    let session = URLSession.shared
+    let (data,response) = try await session.data(for: request)
+
+    guard let httpURLResponse = response as? HTTPURLResponse, 200...300 ~= httpURLResponse.statusCode else { throw RestAPIError.invalidResponse }
+    var decoder = JSONDecoder()
+    let result = try decoder.decode(ResultModel.self, from: data)
+}
