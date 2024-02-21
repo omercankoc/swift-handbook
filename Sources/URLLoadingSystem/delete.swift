88b4928c413bf9c96cdfe50ccc7f05df979f9786
Ömer Koç <63169919+omercankoc@users.noreply.github.com>
2024-02-21 12:54:42 +0300

    Update delete.swift

--- Sources/URLLoadingSystem/delete.swift
+++ Sources/URLLoadingSystem/delete.swift
@@ -1,1 +1,17 @@
+func postImage(endpoint: String, id: String) async throws {
+    guard let url = URL(string: endpoint) else { throw RestAPIError.invalidURL }
 
+    var request = URLRequest(url: url, timeoutInterval: 10)
+    request.addValue(clientCode, forHTTPHeaderField: "ClientCode")
+    request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+    request.addValue(token, forHTTPHeaderField: "Authorization")
+    request.addValue(id, forHTTPHeaderField: "ID")
+    request.httpMethod = "DELETE"
+
+    let session = URLSession.shared
+    let (data,response) = try await session.data(for: request)
+
+    guard let httpURLResponse = response as? HTTPURLResponse, 200...300 ~= httpURLResponse.statusCode else { throw RestAPIError.invalidResponse }
+    var decoder = JSONDecoder()
+    let result = try decoder.decode(ResultModel.self, from: data)
+}
