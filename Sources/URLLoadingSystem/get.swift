9fff085c05db3ff24e73a279b53fc38c56ad5d95
Ömer Koç <63169919+omercankoc@users.noreply.github.com>
2024-02-21 12:06:12 +0300

    Update get.swift

--- Sources/URLLoadingSystem/get.swift
+++ Sources/URLLoadingSystem/get.swift
@@ -1,1 +1,15 @@
-
+func getImages(_ id: String) async throws -> [ImageModel] {
+    guard let url = URL(string: URL) else { throw RestAPIError.invalidURL }
+        
+    var request = URLRequest(url: url, timeoutInterval: 10)
+    request.addValue(clientCode, forHTTPHeaderField: "ClientCode")
+    request.addValue(contentType, forHTTPHeaderField: "Content-Type")
+    request.addValue(token, forHTTPHeaderField: "Authorization")
+    request.httpMethod = "GET"
+        
+    let session = URLSession.shared
+    let (data, response) = try await session.data(for: request)
+        
+    guard let httpURLResponse = response as? HTTPURLResponse, 200...300 ~= httpURLResponse.statusCode else { throw RestAPIError.invalidResponse }
+    return try JSONDecoder().decode([ImageModel].self, from: data)
+}
