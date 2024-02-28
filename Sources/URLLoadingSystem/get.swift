807b31f83bebe8f63fe2c40f32b07dc5551c3737
Ömer Koç <63169919+omercankoc@users.noreply.github.com>
2024-02-28 11:39:09 +0300

    Update get.swift

--- Sources/URLLoadingSystem/get.swift
+++ Sources/URLLoadingSystem/get.swift
@@ -1,4 +1,4 @@
-func getPassenger(endpoint: String) async throws -> [PassengerModel] {
+func getPassengers(endpoint: String) async throws -> [PassengerModel] {
     guard let url = URL(string: endpoint) else { throw RestAPIError.invalidURL }
         
     var request = URLRequest(url: url, timeoutInterval: 10)
